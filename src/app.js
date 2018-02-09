ab6dab0e34c758860983f79de86ddfb6bd9046a3
Royee Shemesh <royee@flexpert.co.il>
2018-02-09 14:47:21 +0200

    Initial Import

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function addListItem() {
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("New LI");
+    node.appendChild(textnode);
+
+    myList.appendChild(node);
+}
+
+addListItem();
+addListItem();
+addListItem();
